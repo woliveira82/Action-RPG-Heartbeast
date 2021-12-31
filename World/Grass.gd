3caeb655e1f4bf73a157443f179e6f79cf0f5afe
Weiner Oliveira <woliveira82@gmail.com>
2021-12-31 16:21:23 -0300

    part 15: enemy death effect + bug fixes

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
+const GrassEffect = preload("res://Effects/GrassEffect.tscn")
+
 
 func create_grass_effect():
-	var GrassEffect = load("res://Effects/GrassEffect.tscn")
 	var grass_effect = GrassEffect.instance()
-	var world = get_tree().current_scene
-	world.add_child(grass_effect)
+	get_parent().add_child(grass_effect)
 	grass_effect.global_position = global_position
 
 
