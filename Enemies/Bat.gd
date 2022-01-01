fd4ca1b721faf6cf5bcd4cecff9c518aacff07e3
Weiner Oliveira <woliveira82@gmail.com>
2021-12-31 22:24:49 -0300

    part 17: player stats + enemy attack

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -20,6 +20,7 @@ var state = CHASE
 onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
+onready var hurtbox = $Hurtbox
 
 
 func _ready():
@@ -35,10 +36,10 @@ func _physics_process(delta):
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
-		
+
 		WANDER:
 			pass
-		
+
 		CHASE:
 			var player = playerDetectionZone.player
 			if player != null:
@@ -47,7 +48,7 @@ func _physics_process(delta):
 			else:
 				state = IDLE
 			sprite.flip_h = velocity.x < 0
-	
+
 	velocity = move_and_slide(velocity)
 
 
@@ -58,7 +59,7 @@ func seek_player():
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	knockback = area.knockback_vector * 120
-
+	hurtbox.create_hit_effect()
 
 func _on_Stats_no_health():
 	queue_free()
