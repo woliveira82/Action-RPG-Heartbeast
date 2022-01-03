671a91a9a1a0f636dd0190c12963b9f898bb5702
Weiner Oliveira <woliveira82@gmail.com>
2022-01-02 21:16:00 -0300

    fix: camera limits + hurtbox bug fix

--- Overlap/Hurtbox.gd
+++ Overlap/Hurtbox.gd
@@ -5,6 +5,7 @@ const HitEffect = preload("res://Effects/HitEffect.tscn")
 var invincible = false setget set_invincible
 
 onready var timer = $Timer
+onready var collisionShape = $CollisionShape2D
 
 signal invincibility_started
 signal invincibility_ended
@@ -35,8 +36,8 @@ func _on_Timer_timeout():
 
 
 func _on_Hurtbox_invincibility_started():
-	monitorable = true
+	collisionShape.set_deferred("disabled", true)
 
 
 func _on_Hurtbox_invincibility_ended():
-	monitorable = true
+	collisionShape.set_deferred("disabled", false)
